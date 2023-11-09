253c64eb6165b9872c735153048a528e4000742a
carbonarangluis <luis.hwtg@gmail.com>
2023-11-09 16:18:28 +0800

    feat create table owners syntax

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,13 @@
-CREATE DATABASE veterinary
+CREATE DATABASE veterinary;
+
+CREATE TABLE owner
+(
+    ownerid INT,
+    ofirstname VARCHAR(50) NOT NULL,
+    olastname VARCHAR(50) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (ownerid)
+);
